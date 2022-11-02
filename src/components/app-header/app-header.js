11d9230912ef64f2770ff44412834c593a9f70eb
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-11-02 16:47:01 +0200

    App structure created

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Artem Balanovskyi</h1>
+            <h2>posts: 5, liked: 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
